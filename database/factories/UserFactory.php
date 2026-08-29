9ece9e6262099a120d13d33e59c2c6fd985b1588
Monami Sadhu <meghalaya4031@gmail.com>
2026-08-29 12:08:57 +0530

    fix: correct UserFactory definition signature

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<User>
@@ -16,22 +16,22 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(Faker $faker): array
+    public function definition(): array
     {
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => bcrypt('password123'),
-            'phone' => $faker->phoneNumber(),
-            'department' => $faker->randomElement([
+            'phone' => $this->faker->phoneNumber(),
+            'department' => $this->faker->randomElement([
                 'Computer Science',
                 'Engineering',
                 'Business',
                 'Arts',
                 'Science'
             ]),
-            'semester' => $faker->randomElement([
+            'semester' => $this->faker->randomElement([
                 '1st',
                 '2nd',
                 '3rd',
@@ -43,8 +43,8 @@ class UserFactory extends Factory
             ]),
             'role' => 'student',
             'status' => 'active',
-            'bio' => $faker->sentence(),
-            'remember_token' => \Illuminate\Support\Str::random(10),
+            'bio' => $this->faker->sentence(),
+            'remember_token' => Str::random(10),
         ];
     }
 }
